f11e45b6a77e28a1ae4834896eac5f50bd202829
William Dunlop <william.dunlop.1993@gmail.com>
2019-05-27 20:55:02 +1000

    began implementing the auth routes, introduced jwtokens

---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+import express from "express";
+import Joi from "joi";
+import bcrypt from "bcrypt";
+import _ from "lodash";
+import jwt from "jsonwebtoken";
+
+import { User } from "../models/users";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send("Invalid email or password");
+    
+        let user = await User.findOne({ email: req.body.email });
+        if (!user) return res.status(400).send("Invalid email or password");
+    
+        const validPassword = await bcrypt.compare(req.body.password, user.hash);
+        if (!validPassword) return res.status(400).send("Invalid email or password");
+
+        const token = jwt.sign({ _id: user._id }, 'secret'); 
+
+        res.send(token);
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+});
+
+function validate(req) {
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    }
+
+    return Joi.validate(req, schema);
+}
+
+
+export default router;
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ import express from "express";
 
 import courses from "./courses";
 import users from "./users";
+import auth from "./auth";
+
 
 const router = express.Router();
 
 router.use("/courses", courses);
 router.use("/users", users);
+router.use("/auth", auth);
 
 export default router;
